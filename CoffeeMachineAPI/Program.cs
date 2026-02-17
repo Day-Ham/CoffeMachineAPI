7e2361a4b426c3bbcfd0aad9df649063439de722
Day-Ham <daehankimm17@gmail.com>
2026-02-17 12:57:13 +0800

    Added The Conditions for Unavailable Service and Teapot Returns

--- CoffeeMachineAPI/Program.cs
+++ CoffeeMachineAPI/Program.cs
@@ -3,8 +3,27 @@ var app = builder.Build();
 const string apiKey= "aefa89ddb40bc09ad524ecaeef1afec1";
 const string city = "Manila"; // Or use lat/lon
 const string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+int totalNumberOfCalls=0;
 app.MapGet("/", () => "Hello Coffee is Ready!");
-app.MapGet("/brew-coffee",()=>"Your Piping hot coffee is ready");
+app.MapGet("/brew-coffee", () =>
+{
+    var DateToday= DateTime.Now;
+    totalNumberOfCalls++;
+    if (totalNumberOfCalls % 5 == 0)
+    {
+        return Results.StatusCode(503);
+    }
+    else if(DateToday.Month==4 && DateToday.Day == 1)
+    {
+        return Results.StatusCode(418);
+    }
+    else
+    {
+    return Results.Ok($"Number of Coffes brewed{totalNumberOfCalls}");
+        
+    }
+
+});
 app.MapGet("/weather", async () =>
 {
     using var client = new HttpClient();
--- CoffeeMachineAPI/obj/Debug/net10.0/CoffeeMachineAPI.AssemblyInfo.cs
+++ CoffeeMachineAPI/obj/Debug/net10.0/CoffeeMachineAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeMachineAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23a09248c499f44c053e7c98721ba7308c3f25e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493ce3f79e9868cff7a892638026ebff869f28de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeMachineAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeMachineAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
